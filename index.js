89ae24b9f3cb4906eefff7533c1ce3b960f4eca3
Josiah Maina <josiah5115@gmail.com>
2018-07-24 11:01:23 -0600

    final filter result

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 let numbers = [3, 5, 2, 5, 7, 7, 3, 4, 4];
 
 // when you make a class your are really making a type - a classification of a new type;
-Array.prototype
-// Number
+Array.prototype.filter = function(callbackFn) {
+  // make new array
+  let newArray = [];
+  let array = this;
+
+    // loop through the array
+  for (let i = 0; i < array.length; i++) {
+    const element= array[i];
+
+      // invock callback passing in item from array
+      const passesFilter = callbackFn(element);
+        // if return is true,
+        if (passesFilter) {
+          // Add to new array;
+          newArray.push(element);
+       };
+};
+return newArray
+};
+// higher-order function only knows how to do part of the function; so we have to pass a callback function/ another function that does the rest of the job;
+
+
+// types/ class/ classification;
+// Number 
+// const num = new Number(7) is the same as 
+// const num = 7;
 // Object
 
 
