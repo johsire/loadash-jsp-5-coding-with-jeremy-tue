c361af8a90ae6a272a17214a67d19008270c5f9b
Josiah Maina <josiah5115@gmail.com>
2018-07-24 10:46:55 -0600

    class are classification and are types

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 let numbers = [3, 5, 2, 5, 7, 7, 3, 4, 4];
 
+// when you make a class your are really making a type - a classification of a new type;
+Array.prototype
+// Number
+// Object
+
+
 let evens = numbers.filter(x => x % 2 === 0)
 // evens; [2, 4, 4]
+
+// filter /map / reduce/ sort / is a higher order function;
